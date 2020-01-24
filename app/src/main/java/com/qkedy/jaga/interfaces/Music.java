a650dfe9d1e574b46efbd6a8a34f4887b4c21773
ali.k <sk4015620@gmail.com>
2020-01-23 18:47:22 -0800

    Add: Music.java (every game needs music)

---
+++ app/src/main/java/com/qkedy/jaga/interfaces/Music.java
@@ -0,0 +1,24 @@
+package com.qkedy.jaga.interfaces;
+
+public interface Music {
+
+    void play();
+
+    void stop();
+
+    void pause();
+
+    void setLooping(boolean looping);
+
+    void setVolume(float volume);
+
+    boolean isPlaying();
+
+    boolean isStopped();
+
+    boolean isLooping();
+
+    void dispose();
+
+    void seekBegin();
+}
