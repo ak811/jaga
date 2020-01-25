5a991c6bea2ce1ee52b4a336da9d62e02d10fb56
ali.k <sk4015620@gmail.com>
2020-01-25 14:44:23 -0800

    Add: AndroidMusic.java for implementation of Music methods

---
+++ app/src/main/java/com/qkedy/jaga/implementation/AndroidMusic.java
@@ -0,0 +1,131 @@
+package com.qkedy.jaga.implementation;
+
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.media.MediaPlayer.OnSeekCompleteListener;
+import android.media.MediaPlayer.OnVideoSizeChangedListener;
+
+import androidx.annotation.NonNull;
+
+import com.qkedy.jaga.interfaces.Music;
+
+import java.io.IOException;
+
+public class AndroidMusic implements Music, OnCompletionListener, OnSeekCompleteListener, OnPreparedListener, OnVideoSizeChangedListener {
+
+    private MediaPlayer mediaPlayer;
+    private boolean isPrepared;
+
+    public AndroidMusic(@NonNull AssetFileDescriptor assetDescriptor) {
+        mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource(assetDescriptor.getFileDescriptor(), assetDescriptor.getStartOffset(), assetDescriptor.getLength());
+            mediaPlayer.prepare();
+            isPrepared = true;
+            mediaPlayer.setOnCompletionListener(this);
+            mediaPlayer.setOnSeekCompleteListener(this);
+            mediaPlayer.setOnPreparedListener(this);
+            mediaPlayer.setOnVideoSizeChangedListener(this);
+        } catch (Exception e) {
+            throw new RuntimeException("Couldn't load music");
+        }
+    }
+
+    @Override
+    public void dispose() {
+        if (this.mediaPlayer.isPlaying())
+            this.mediaPlayer.stop();
+        this.mediaPlayer.release();
+    }
+
+    @Override
+    public boolean isLooping() {
+        return mediaPlayer.isLooping();
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return this.mediaPlayer.isPlaying();
+    }
+
+    @Override
+    public boolean isStopped() {
+        return !isPrepared;
+    }
+
+    @Override
+    public void pause() {
+        if (this.mediaPlayer.isPlaying())
+            mediaPlayer.pause();
+    }
+
+    @Override
+    public void play() {
+        if (this.mediaPlayer.isPlaying())
+            return;
+
+        try {
+            synchronized (this) {
+                if (!isPrepared)
+                    mediaPlayer.prepare();
+                mediaPlayer.start();
+            }
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void setLooping(boolean isLooping) {
+        mediaPlayer.setLooping(isLooping);
+    }
+
+    @Override
+    public void setVolume(float volume) {
+        mediaPlayer.setVolume(volume, volume);
+    }
+
+    @Override
+    public void stop() {
+        if (this.mediaPlayer.isPlaying()) {
+            this.mediaPlayer.stop();
+            synchronized (this) {
+                isPrepared = false;
+            }
+        }
+    }
+
+    @Override
+    public void onCompletion(@NonNull MediaPlayer player) {
+        synchronized (this) {
+            isPrepared = false;
+        }
+    }
+
+    @Override
+    public void seekBegin() {
+        mediaPlayer.seekTo(0);
+
+    }
+
+    @Override
+    public void onPrepared(@NonNull MediaPlayer player) {
+        synchronized (this) {
+            isPrepared = true;
+        }
+    }
+
+    @Override
+    public void onSeekComplete(@NonNull MediaPlayer player) {
+        // todo: should complete
+    }
+
+    @Override
+    public void onVideoSizeChanged(MediaPlayer player, int width, int height) {
+        // todo: should complete
+    }
+}
